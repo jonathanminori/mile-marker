cf9aa2075f0bf4961f52a0a015d7903d19be86c4
jonathanminori <jonathan.minori@gmail.com>
2015-08-15 22:13:23 -0700

    Up to Pagination

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,20 +6,37 @@ Template.postSubmit.events({
 			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
 		};
+		
+		var errors = validatePost(post);
+		if (errors.title || errors.url)
+			return Session.set('postSubmitErrors', errors);
 
 		Meteor.call('postInsert', post, function(error, result) {
 			// display the error to the user and abort
 			if (error) {
-				return alert(error.reason);
-				Router.go('postsPage', {_id: result._id});
+				return throwError(error.reason);
+				//Router.go('postsPage', {_id: result._id});
 			}
 			
 			// show this result but route anyway
 			if (result.postExists) {
-				alert('This link has already been posted');
+				throwError('This link has already been posted');
 			}
 		});
 		
 		Router.go('postsList');
+	}
+});
+
+Template.postSubmit.onCreated(function() {
+	Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+	errorMessage: function(field) {
+		return Session.get('postSubmitErrors')[field];
+	},
+	errorClass: function(field) {
+		return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
 	}
 });
